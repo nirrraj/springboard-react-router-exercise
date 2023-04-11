f9ca45a009035311e779ae130ff468d2b11c301c
Niraj Singh <119704924+nirrraj@users.noreply.github.com>
2023-04-11 12:57:27 -0400

    Update Cookies.js

--- src/Cookies.js
+++ src/Cookies.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Cookies = () => {
   return (
     <div className='container'>
-      <h1>Cookies</h1>
+      <h1>🍪 Cookies</h1>
       <p>A delicious bag of chocolate chip cookies.</p>
       <Link to="/">Back to Vending Machine</Link>
     </div>
