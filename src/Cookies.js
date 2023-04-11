b681d44c46c6a9ff9c75132da18dfd7fe974a095
Niraj Singh <119704924+nirrraj@users.noreply.github.com>
2023-04-11 12:57:05 -0400

    Create Cookies.js

---
+++ src/Cookies.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Cookies = () => {
+  return (
+    <div className='container'>
+      <h1>Cookies</h1>
+      <p>A delicious bag of chocolate chip cookies.</p>
+      <Link to="/">Back to Vending Machine</Link>
+    </div>
+  );
+};
+
+export default Cookies;
